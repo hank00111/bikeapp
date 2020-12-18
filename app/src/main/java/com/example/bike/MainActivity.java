97c4c4888db00752328e714299a629453a6a98b5
hank00111 <42414986+hank00111@users.noreply.github.com>
2020-12-18 23:32:56 +0800

    v1.0.2
    Add 2 thread
    Add numpick
    Add wifi text
    2020/12/18

--- app/src/main/java/com/example/bike/MainActivity.java
+++ app/src/main/java/com/example/bike/MainActivity.java
@@ -8,11 +8,15 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.net.wifi.WifiManager;
+import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.net.Socket;
@@ -23,13 +27,27 @@ import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    WifiManager mWifiManager;
-    WifiInfo wifiInfo;
-    Button bt1, bt2, bt3, bt4;
-    TextView tv1;
-    Socket socket;
+    private static WifiManager mWifiManager;
+
+    private WifiInfo wifiInfo;
+    private Button bt1, bt2, bt3, bt4;
+    private TextView tv1;
+    private Socket socket;
+    private String t;
+    private int i,j ,x;
+
+    private  static  final  String TAG = "MainActivity";
+
+    private Handler mainHandler = new Handler();
+    private Handler conHandler = new Handler();
+
+    private  volatile boolean stpthred = false;
+
+    private Thread thread1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +59,22 @@ public class MainActivity extends AppCompatActivity {
         bt2 = findViewById(R.id.Bt2);
         bt3 = findViewById(R.id.Bt3);
         bt4 = findViewById(R.id.Bt4);
+        NumberPicker numberPicker = findViewById(R.id.number_picker);
+        String[] values = {"Bike", "Low", "Mid", "High"};
+        numberPicker.setDisplayedValues(values);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(3);
+        numberPicker.setOnValueChangedListener(onValueChangeListener);
+
     }
+    NumberPicker.OnValueChangeListener onValueChangeListener =
+            new NumberPicker.OnValueChangeListener(){
+                @Override
+                public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+                    tv1.setText("selected number " + numberPicker.getValue());
+                }
+            };
+
 
     public void getGPSpermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -49,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
     }
+
 
     public void onClic1(View view) {
         mWifiManager = (WifiManager) MainActivity.this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -83,7 +117,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClic3(View view) {
         setbtn(3);
-        new Thread() {
+
+        stpthred = true;
+
+        thread1.interrupt();
+        /*new Thread() {
             public void run() {
                 try {
                     //Socket socket = new S
@@ -98,10 +136,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }.start();
+        */
     }
 
     public void onClic4(View view) {
         setbtn(4);
+        stpthred = false;
+        ExRunnable runnable = new ExRunnable(20);
+        new  Thread(runnable).start();
+
+        thread1 = new Thread(){
+            @Override
+            public void run() {
+                for (j = 0; j<1000;j++){
+                    if (stpthred){
+                        Log.d(TAG,"Close Thread1");
+                        return;
+                    }
+                    try {
+                        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                        t =format.format(new Date());
+                        sleep(1000);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    x+=1;
+                    if (x==4)
+                        x=0;
+                    conHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String [] strings = {"",".", ".." ,"..."};
+                            tv1.setText("Connecting" + strings[x]);
+
+                        }
+                    });
+
+                    Log.d(TAG,t+" "+j +" "+x);
+                }
+            }
+        };
+        thread1.start();
+        //ExThread thread = new ExThread(10);
+        //thread.start();
     }
 
     public void setbtn(int ty) {
@@ -152,5 +229,46 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    class  ExRunnable implements  Runnable{
+        int sec;
+
+        ExRunnable(int sec){
+            this.sec = sec;
+        }
+
+        @Override
+        public void run() {
+            for ( i = 1; i <= sec; i++) {
+                if (stpthred){
+                    Log.d(TAG,"Close Thread");
+                    return;
+                }
+
+                try {
+                    if (i == 5) {
+                        mainHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv1.setText("5");
+                            }
+                        });
+                    }
+                    //SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                    //String t =format.format(new Date());
+                    Log.d(TAG, "connecting: " + i);
+                    socket = new Socket("192.168.4.1", 4567);
+
+
+                    Thread.sleep(50);
+                } catch (Exception e) {
+                    Log.i("MainActivity", "Faile");
+                    //e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 
 }
+
